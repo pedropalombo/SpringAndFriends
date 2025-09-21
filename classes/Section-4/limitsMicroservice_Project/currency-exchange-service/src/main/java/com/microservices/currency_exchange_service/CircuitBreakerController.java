06b0bf97ed1c3fca70d16813d502ef9e9ec22f09
pedropal <pedropal@amdocs.com>
2025-09-20 22:47:51 -0300

    Twelfth Step - Rate Limiting, Bulkhead

--- classes/Section-4/limitsMicroservice_Project/currency-exchange-service/src/main/java/com/microservices/currency_exchange_service/CircuitBreakerController.java
+++ classes/Section-4/limitsMicroservice_Project/currency-exchange-service/src/main/java/com/microservices/currency_exchange_service/CircuitBreakerController.java
@@ -8,7 +8,9 @@ import org.springframework.web.client.RestTemplate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
 
 // class to manage the 'Circuit Breaker' logic
@@ -20,7 +22,10 @@ public class CircuitBreakerController {
 	
 	// sample API to be used
 	@GetMapping(path = "/sample-api")
-	@Retry(name = "sample-api", fallbackMethod="hardcodedResponse")	// setting the name of the API for configuring the amount of retries on 'application.properties'
+	//@Retry(name = "sample-api", fallbackMethod="hardcodedResponse")	// setting the name of the API for configuring the amount of retries on 'application.properties'
+	@CircuitBreaker(name = "default", fallbackMethod="hardcodedResponse") // enveloping the method's REST operation in its pattern
+	@RateLimiter(name = "default")		// used for limiting the number of received requests for a given period (eg: 2s)
+	@Bulkhead(name = "default")		// enables/disables concurrent requests to be done to an API
 	public String sampleApi() {
 		
 		// logging the info to the console
--- classes/Section-5/3-CircuitBreaker.md
+++ classes/Section-5/3-CircuitBreaker.md
@@ -2,7 +2,18 @@
 )) can be found in 'currency-exchange-service' ))
 
 > prevents system outage/slow-down in case a microservice is down/slow
-    ^-!> they have to be independent, right? ;)
+    \-!> they have to be independent, right? ;)
+    \-> through annotations
+        ^-> it envelops a method/URL, and protects it from attacks, critical failures, etc.
+            +->  [it allows request if all is fine, blocks them if not, and allow some when failures happened but not anymore](/resources/section5/circuitBreaker.png)
+                )-> also known as the 3 states [CLOSED, OPEN, HALF_OPEN](https://resilience4j.readme.io/docs/circuitbreaker)
 
 > also know as "Resilience" ("Resilience4J")
     ^-!> PS: needs a dependency + controller
+
+## Rate Limiting ##
+> limits the rate (amount x times) that the requests can come in
+    \-!> PS: it's an annotation, so it uses configuration from 'application.properties'
+
+> Bulkhead
+    \-> allows concurrent requests to be receive, and defines how many are possible at a time
